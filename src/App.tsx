40be391f6d66d1c2bbed5772e5a53b292ae835f2
Mark Edward Ratio <markedwardratio@gmail.com>
2022-01-06 19:05:05 +0800

    Add /patients/:patientId view

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from "react";
-import axios from "axios";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import { Button, Divider, Header, Container } from "semantic-ui-react";
+import React from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { Button, Divider, Header, Container } from 'semantic-ui-react';
 
-import { apiBaseUrl } from "./constants";
-import { useStateValue } from "./state";
-import { Patient } from "./types";
+import { apiBaseUrl } from './constants';
+import { useStateValue } from './state';
+import { Patient } from './types';
 
-import PatientListPage from "./PatientListPage";
+import PatientListPage from './PatientListPage';
+import PatientPage from './components/PatientPage';
 
 const App = () => {
   const [, dispatch] = useStateValue();
+
   React.useEffect(() => {
     void axios.get<void>(`${apiBaseUrl}/ping`);
 
@@ -19,7 +21,7 @@ const App = () => {
         const { data: patientListFromApi } = await axios.get<Patient[]>(
           `${apiBaseUrl}/patients`
         );
-        dispatch({ type: "SET_PATIENT_LIST", payload: patientListFromApi });
+        dispatch({ type: 'SET_PATIENT_LIST', payload: patientListFromApi });
       } catch (e) {
         console.error(e);
       }
@@ -37,8 +39,11 @@ const App = () => {
           </Button>
           <Divider hidden />
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <PatientListPage />
+            </Route>
+            <Route exact path="/patients/:patientId">
+              <PatientPage />
             </Route>
           </Switch>
         </Container>
---
+++ src/components/PatientPage.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { Container, Icon } from 'semantic-ui-react';
+
+import { apiBaseUrl } from '../constants';
+import { useStateValue } from '../state';
+import { Patient } from '../types';
+
+const PatientPage = () => {
+	const [{ patient }, dispatch] = useStateValue();
+	const { patientId } = useParams<{ patientId: string }>();
+	const [isLoading, setIsLoading] = React.useState<boolean>(true);
+
+	const getGenderIcon = (gender: string) => {
+		if (gender === 'male') {
+			return <Icon name="mars" />;
+		} else if (gender === 'female') {
+			return <Icon name="venus" />;
+		} else {
+			return <Icon name="genderless" />;
+		}
+	};
+
+	React.useEffect(() => {
+		const fetchPatient = async () => {
+			if (patient === undefined || patient.id !== patientId) {
+				try {
+					const { data: patientFromApi } = await axios.get<Patient>(
+						`${apiBaseUrl}/patients/${patientId}`
+					);
+					dispatch({ type: 'SET_PATIENT', payload: patientFromApi });
+				} catch (e) {
+					console.error(e);
+				}
+			}
+			setIsLoading(false);
+		};
+		void fetchPatient();
+	}, [patientId]);
+
+	if (patient === undefined || isLoading) {
+		return <div>loading...</div>;
+	}
+
+	return (
+		<div className="App">
+			<Container>
+				<h3>
+					{patient.name} {getGenderIcon(patient.gender)}
+				</h3>
+				<span>ssn:{patient.ssn}</span>
+				<br></br>
+				<span>occupation:{patient.occupation}</span>
+			</Container>
+		</div>
+	);
+};
+
+export default PatientPage;
